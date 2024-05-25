d010d3d41ae08bf5336087d4b3c0b6c281abad01
Timothe Watteau <timothe.wt@gmail.com>
2024-05-25 23:15:01 +0200

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -9,8 +9,8 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('src/'))
-sys.path.insert(1, os.path.abspath('../library'))
+sys.path.insert(0, os.path.abspath('../src/'))
+sys.path.insert(1, os.path.abspath('../library/'))
 
 project = 'Advanced Graph Clustering'
 author = 'Timothe Watteau, Joaquim Jusseau, Aubin Bonnefoy, Simon Illouz-Laurent'
